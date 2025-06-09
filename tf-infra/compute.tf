d80bbc0bd6cad225ad263c6729c2e9b8565d0df9
Richard Grime <richardgrime@google.com>
2025-06-09 13:26:46 +0000

    Bump component versions and Guacamole to 1.5.5

--- tf-infra/compute.tf
+++ tf-infra/compute.tf
@@ -64,7 +64,7 @@ resource "google_compute_instance" "db-management" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-10"
+      image = "debian-cloud/debian-12"
     }
   }
 
